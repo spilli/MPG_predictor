d04f25c6dc5cea23d72d38192ee1b361b09a1535
Sridhar Pilli <spilli27@gmail.com>
2016-04-01 11:45:38 -0700

    updated model with slidify

--- server.R
+++ server.R
@@ -3,55 +3,36 @@ library(shiny)
 # Fit a linear regression model on the mtcars dataset from the datasets package. 
 # This section runs only once when the app starts
 data(mtcars)
-fit <- lm(mpg ~ cyl + hp + wt + disp + am + disp:cyl + cyl:hp + cyl:wt + am:hp ,mtcars)
+fit <- lm(mpg ~ disp + am + carb ,mtcars)
 
 shinyServer(
     function(input, output) {
-        #output$cyl <- renderPrint({input$cyl})
-        #output$hp <- renderPrint({input$hp})
-        #output$wt <- renderPrint({input$wt})
-        #output$disp <- renderPrint({input$disp})
-        #output$am <- renderPrint({input$am})
-        #output$fitSum <- renderPrint({summary(fit)})
         
         output$idf <- renderTable({ 
-            data.frame(cylinders = input$cyl,
-                       hp = input$hp,
-                       weight = input$wt,
-                       displacement = input$disp,
-                       transmission = input$am
+            data.frame(displacement = input$disp,
+                       transmission = input$am,
+                       carburetors = input$carb
             ) 
         })
         
         am <- reactive({ ifelse(input$am == "Automatic",0,1) })
-        wt <- reactive({ input$wt/1000 })
-        df <- reactive({ 
-            data.frame(cyl = input$cyl,
-                       hp = input$hp, 
-                       wt = wt(),
-                       disp = input$disp,
-                       am = am())
-        })
+        df <- reactive({ data.frame(disp = input$disp,am = am(), carb = input$carb) })
         
         predMpg <- reactive({ predict(fit,df(),levels=0.95, interval = "confidence") })
         output$pred <- renderText({ round(predMpg()[1],2) })
         output$confInt <- renderPrint({ round(predMpg()[2:3],2) })
         
         output$myplot <- renderPlot({
-            par(mfrow=c(2,2),mar=c(4,4,1,1))
-            with(mtcars,plot(mpg~hp));abline(lm(mpg~hp,mtcars),lwd=3)
-            abline(v=input$hp,lwd=5,col="red")
-            
-            with(mtcars,plot(mpg~I(wt*1000),xlab="wt"));abline(lm(mpg~I(wt*1000),mtcars),lwd=3)
-            abline(v=input$wt,lwd=5,col="red")
+            par(mfrow=c(1,2),mar=c(4,4,2,1))
             
             with(mtcars,plot(mpg~disp));abline(lm(mpg~disp,mtcars),lwd=3)
             abline(v=input$disp,lwd=5,col="red")
             
-            with(mtcars,plot(mpg~cyl));abline(lm(mpg~cyl,mtcars),lwd=3)
-            abline(v=input$cyl,lwd=5,col="red")
+            with(mtcars,plot(mpg~carb));abline(lm(mpg~carb,mtcars),lwd=3)
+            abline(v=input$carb,lwd=5,col="red")
         })
         
+        output$fitSum <- renderPrint(summary(fit))
 
     } 
 )
--- ui.R
+++ ui.R
@@ -2,46 +2,35 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("Miles Per Gallon Predictor"), 
     sidebarPanel(
-        h4('Enter the details of your concept car'),
-        numericInput('cyl','Number of Cylinders',4,min=4,max=8,step=2),
-        sliderInput('hp','Gross Horse Power (hp)',value=110,min=50,max=300,step=1),
-        sliderInput('wt','Vehicle weight (lbs)',2000,min=1400,max=5000,step=10),
+        h4('Enter the details of your car'),
         sliderInput('disp','Engine displacement (cu. in.)',200,min=100,max=400,step=1),
         selectInput("am","Transmission type",c("Automatic" = "Automatic", "Manual" = "Manual")),
+        numericInput('carb','Number of Carburetors',4,min=1,max=8,step=1),
         tags$br(),tags$br(),tags$br(),
         p('Author : Sridhar Pilli'),
         p('Email  : spilli27@gmail.com')
     ),
     mainPanel(
-        h4('This application lets you predict the miles per gallon of a concept car based
-           on the number of cylinders, horse power, vehicle weight, engine displacement and 
-           the type of transmission.'),
-
-        tags$br(),
-        h4('Description of input parameters'),
-        h6('Number of cylinders can be either 4 or 6 or 8.'),
-        h6('Horse power can be any value from 50hp to 350hp in steps of 1hp.'),
-        h6('Weight of the vehicle can be 1000 to 5000 lbs in steps of 1lb.'),
-        h6('Engine displacement can be 100 cu.in. to 400 cu.in in steps of 1 cu.in. '),
-        h6('Transmission type can be either Automatic or Manual.'),
+        h4('Summary'),
+        h6('This application lets you predict the miles per gallon of a  car based
+           on the engine displacement, the type of transmission and the number of carbureters.
+           Since the predictive model is based on the data from datasets package in R from 
+           the 70s, any predicted mpg would map to the one from that era.'),
         
         tags$br(),
         h4('Input values you entered'),
         tableOutput("idf"),
-        plotOutput("myplot"),
-
-        #h4('The input values are sent to the following linear regression model'),
-        #verbatimTextOutput("fitSum"),
+        plotOutput("myplot",width="100%",height = "200px"),
         
-        tags$br(),
         h4('The predicted miles per gallon is'),
         verbatimTextOutput("pred"),
         h4('The 95% confidence interval of the predicted mpg is'),
         verbatimTextOutput("confInt"),
         
-        tags$br(),
-        p('The model used here to predict the mpg is a linear regression model fitted 
-            on the mtcars dataset from the datsets package in R. ')
+        h4('Linear regression model'),
+        verbatimTextOutput("fitSum")
+        
+
         
     ) 
 ))
